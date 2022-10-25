67650889b9ce2487bb5f40920b3a1164a55e5204
shjanken <janken.wang@gmail.com>
2022-10-25 15:16:09 +0800

    [feature] 添加取消自动启动的功能
    删除 startup 中的文件。放入回收站

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ def resource_path(relative_path):
 class AutoStarter:
     """ Get or sett the program autostart status."""
 
-    statue: bool = False
+    is_auto_start: bool = False
 
     __program_no_running_error = "没有找到程序运行信息"
 
     def __init__(self, wmi_client: wmi.WMI):
         self.wmi_client = wmi_client
-        self.statue = (Path(STARTUP_FOLDER) / PROGRAM_NAME).exists()
+        self.is_auto_start = (Path(STARTUP_FOLDER) / PROGRAM_NAME).exists()
 
     def search_program_path(self) -> str:
         """use wmi win32_process find the program process.
@@ -51,9 +51,16 @@ class AutoStarter:
         try:
             p = self.search_program_path()
             winshell.copy_file(p, winshell.folder("startup"), no_confirm=True)
-            self.statue = True  # copy file success. the program is auto started
+            self.is_auto_start = True  # copy file success. the program is auto started
         except OSError:
             raise
+
+    def unset_auto_start(self) -> None:
+        """Delete the file from user's startup folder"""
+        p = Path(STARTUP_FOLDER) / PROGRAM_NAME
+        if p.exists() and self.is_auto_start:
+            winshell.delete_file(f"{p.resolve().absolute()}", no_confirm=True, silent=True)
+            self.is_auto_start = False
 
 
 class MainPanel:
@@ -99,7 +106,11 @@ class MainPanel:
 
     def __set_auto_start(self):
         try:
-            self.starter.set_auto_start()
+            if not self.starter.is_auto_start:
+                self.starter.set_auto_start()
+            else:
+                self.starter.unset_auto_start()
+
         except OSError as ex:
             if self.icon.HAS_NOTIFICATION:
                 self.icon.notify(f"创建开机启动失败：{ex}", "错误")
@@ -110,7 +121,7 @@ class MainPanel:
         icon_img = Image.open(resource_path('assets/tray_32x.ico'))
         menu = (MenuItem("显示", self.__show_window),
                 MenuItem("自启", self.__set_auto_start,
-                         checked=lambda i: self.starter.statue),
+                         checked=lambda _: self.starter.is_auto_start),
                 MenuItem(Menu.SEPARATOR, None),
                 MenuItem("退出", self.__destroy_window))
         self.icon = Icon("info", icon_img, menu=menu)
